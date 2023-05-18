0f84c965c63269c771243d436e8309e3d96d7319
lourdel <klourdel@gmail.com>
2023-05-18 05:57:40 +0300

    lru cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module defines LRU cache"""
+
+from datetime import datetime
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Method implements the LRU cache"""
+
+    def __init__(self):
+        self.stack = list()
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache memory
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS\
+                    and key not in self.stack:
+                discard_key = self.stack.pop(0)
+                del self.cache_data[discard_key]
+                print("DISCARD: {}".format(discard_key))
+            if key in self.stack:
+                self.stack.remove(key)
+            self.cache_data.update({key: item})
+            self.stack.append(key)
+
+    def get(self, key):
+        """Returns a value of a particular key from
+                self.cache_data dictionary"""
+        if key in self.stack:
+            self.stack.remove(key)
+            self.stack.append(key)
+        return self.cache_data.get(key)
